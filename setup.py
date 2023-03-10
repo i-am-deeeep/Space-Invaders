1af8fca5dc6d7d38c8f620fced1126f2be6c337c
Subhodeep Kumar Singh <subhodeep2791999@gmail.com>
2023-03-11 01:14:00 +0530

    everything done

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ executables = [cx_Freeze.Executable("main.py")]
 cx_Freeze.setup(
     name="Space Invaders",
     options={"build_exe": {"packages":["pygame","pygame.mixer","pygame.time","random","math"],
-                           "include_files":["alien1.png","alien2.png","alien3.png","alien4.png","alien5.png","alien6.png","background-music.wav","background.png","bullet.png","crash.wav","enemy_bullet.png","explosion.wav","game-over.wav","icon.png","IndieFlower-Regular.ttf","laser.wav","README.md","spaceship.png","spaceship_dead.png"]}},
+                           "include_files":["exee.txt","alien1.png","alien2.png","alien3.png","alien4.png","alien5.png","alien6.png","background-music.wav","background.png","bullet.png","crash.wav","enemy_bullet.png","explosion.wav","game-over.wav","icon.png","IndieFlower-Regular.ttf","laser.wav","README.md","spaceship.png","spaceship_dead.png"]}},
     executables = executables
 
     )
